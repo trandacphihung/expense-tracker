ac6c25d1cd46b00f2c7e02016e753c442b68b1b2
Dac Phi Hung Tran <hung.tran-dxc@hpe.com>
2023-08-23 22:08:15 +0700

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Expenses from './components/Expenses';
 import { expenses } from './data/dummyData';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   return (
     <>
+      <NewExpense />
       <Expenses expenses= {expenses}/>
     </>
   );
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,15 +1,19 @@
 import { ExpensesItemProp } from '../modals/modals';
 import ExpensesDate from './ExpenseDate';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
 function ExpensesItem({
   title,
   amount,
   date,
 }: ExpensesItemProp) {
-  const clickHandler = () => {
-    console.log('Clicked!');
-  };
+  const [newTitle, setNewTitle] = useState(title);
+
+  function clickHandler() {
+    setNewTitle('Updated!!!');
+    console.log(newTitle);
+  }
   return (
     <>
       <div className="expense-item">
@@ -17,7 +21,7 @@ function ExpensesItem({
           date= {date}
         />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{newTitle}</h2>
           <div className="expense-item__price">${amount}</div>
           <button onClick={clickHandler}>Change Title</button>
         </div>
